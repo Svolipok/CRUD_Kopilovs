f7f79106dbf09f534a02cfc3c11ceca9c53a500c
Dmitrijs <svolipok@gmail.com>
2024-12-30 11:48:14 +0200

    added ApplicationDbContext

--- CRUD_Kopilovs/Data/ApplicationDbContext.cs
+++ CRUD_Kopilovs/Data/ApplicationDbContext.cs
@@ -1,6 +1,33 @@
-﻿namespace CRUD_Kopilovs.Data
+﻿using Microsoft.EntityFrameworkCore;
+using CRUD_Kopilovs.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace CRUD_Kopilovs.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Настройка связей
+            modelBuilder.Entity<Order>()
+                .HasOne(o => o.Client)
+                .WithMany(c => c.Orders)
+                .HasForeignKey(o => o.ClientId);
+
+            modelBuilder.Entity<Order>()
+                .HasOne(o => o.Product)
+                .WithMany()
+                .HasForeignKey(o => o.ProductId);
+        }
     }
 }
